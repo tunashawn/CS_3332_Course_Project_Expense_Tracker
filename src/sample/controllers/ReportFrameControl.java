46d71efc9ea3bada5f57ea4010d956ea6bad52bf
Tuna Shawn <tunashawnv@gmail.com>
2021-08-14 01:12:06 +0700

    working on reports

--- src/sample/controllers/ReportFrameControl.java
+++ src/sample/controllers/ReportFrameControl.java
@@ -5,8 +5,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.*;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.layout.AnchorPane;
+import sample.Main;
+import sample.models.Categories;
+import sample.models.Transactions;
 import sample.models.Wallets;
+import sample.utils.Utils;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -15,14 +21,21 @@ public class ReportFrameControl {
 
     @FXML private BarChart<String, Number> bar_chart;
     @FXML private PieChart left_pie, right_pie;
-    @FXML private ComboBox<String> time_range_combobox, wallet_combobox;
+    @FXML private ComboBox<String> wallet_combobox, time_range_combobox;
+    @FXML private AnchorPane pane;
+    @FXML private Button back_button, forward_button;
+
 
     private ArrayList<Wallets> walletList;
     private Wallets selectedWallet;
+    private ArrayList<Transactions> transactionList;
+    private ArrayList<Transactions> expenseList;
+    private ArrayList<Transactions> incomeList;
 
     public ReportFrameControl(Wallets selectedWallet, ArrayList<Wallets> walletList) {
         this.walletList = walletList;
         this.selectedWallet = selectedWallet;
+        this.transactionList = selectedWallet.getTransactionList();
     }
 
 
@@ -67,6 +80,38 @@ public class ReportFrameControl {
             wallet_names.add(wallets.getName());
         }
         Collections.sort(wallet_names);
+        for (String wallet_name : wallet_names) {
+            wallet_combobox.getItems().add(wallet_name);
+        }
+        wallet_combobox.getSelectionModel().select(selectedWallet.getName());
+    }
+
+    private void populateBarChart(){
+        expense_bar.setName("Expense");
+        for (Transactions transaction : expenseList) {
+            expense_bar.getData().add(new XYChart.Data<String, Number>("Jan", -245));
+
+        }
+
+
+        income_bar.setName("Income");
+        income_bar.getData().add(new XYChart.Data<String, Number>("Jan", 270));
+    }
+
+    private void separateIncomeAndExpense(){
+        for (Transactions transactions : transactionList) {
+            if (transactions.getAmount() >= 0)
+                incomeList.add(transactions);
+            else expenseList.add(transactions);
+        }
+    }
+
+    private void populateTimeRangeCombobox(){
+        time_range_combobox.getItems().add("Month");
+        time_range_combobox.getItems().add("Quarter");
+        time_range_combobox.getItems().add("Year");
+        time_range_combobox.getItems().add("All");
+        time_range_combobox.getSelectionModel().select("Month");
     }
 }
 
